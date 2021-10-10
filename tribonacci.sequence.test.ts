ba6ee8f50223bf314ea2a2877795910e3f9316d8
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-10-10 20:21:35 -0300

    It fixes the line 5.

--- tribonacci.sequence.test.ts
+++ tribonacci.sequence.test.ts
@@ -2,7 +2,7 @@ import {tribonacci} from './tribonacci.sequence';
 
 describe('Tests for Tribonacci sequence', () => 
 {
-    test('It should receive [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]', () => 
+    test('It should return [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]', () => 
     {
         expect(tribonacci([1, 1, 1], 10)).toStrictEqual([1, 1, 1, 3, 5, 9, 17, 31, 57, 105]);
     });    
